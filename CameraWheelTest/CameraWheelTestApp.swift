5877083e4d267fbd6e3bbd65748a0fb6b8ada54f
Michael Cadet <mcadet@saiyasoft.com>
2025-08-13 22:19:45 -0400

    Initial Commit

---
+++ CameraWheelTest/CameraWheelTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraWheelTestApp.swift
+//  CameraWheelTest
+//
+//  Created by Michael Cadet on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraWheelTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
